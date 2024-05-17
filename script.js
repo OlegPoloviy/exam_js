29da7adfb3bed439ac43b44b597d3fa1d4f23285
OlegPoloviy <olehtratata@gmail.com>
2024-05-17 14:20:30 +0300

    додано перевірку на реєстрацію

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
- // get currency using fetch
-let currencyButton = document.getElementsByClassName("course2")[0]
- let priceText = document.getElementsByClassName("selected-currency")[0]
- let currency = document.getElementById('curr')
+import { loggedIn } from './registration.js';
 
- currencyButton.onclick = () => {
-     let selectedCurrency = currency.value
-     priceText.textContent = `${selectedCurrency} in UAH`
-     if (selectedCurrency === "USD"){
-         fetch('https://api.monobank.ua/bank/currency')
-             .then(response => {
-                 return response.json()
-             })
+// get currency using fetch
+let currencyButton = document.getElementsByClassName("course2")[0];
+let priceText = document.getElementsByClassName("selected-currency")[0];
+let currency = document.getElementById('curr');
 
-             .then(data => {
-                let price = document.getElementsByClassName('price')[0]
-                 price.innerHTML = data[0].rateBuy
-             })
-             .catch(err => {
-                 console.error("error fetching data : ",err)
-             })
-     }else{
-         fetch('https://api.monobank.ua/bank/currency')
-             .then(response => response.json())
-             .then(data => {
-                 let price = document.getElementsByClassName('price')[0]
-                 price.innerHTML = data[1].rateBuy
-             })
-             .catch(err => {
-                 console.error("error fetching data : ",err)
-             })
-     }
-
- }
+currencyButton.onclick = () => {
+    if (loggedIn === false) {
+        let money = document.getElementsByClassName('money')[0];
+        money.innerText = 'You need to be logged in!';
+        money.style.color = 'red';
+        money.style.marginTop = '40px';
+    } else if (loggedIn === true) {
+        let selectedCurrency = currency.value;
+        priceText.textContent = `${selectedCurrency} in UAH`;
+        if (selectedCurrency === "USD") {
+            fetch('https://api.monobank.ua/bank/currency')
+                .then(response => response.json())
+                .then(data => {
+                    let price = document.getElementsByClassName('price')[0];
+                    price.innerHTML = data[0].rateBuy;
+                })
+                .catch(err => {
+                    console.error("error fetching data : ", err);
+                });
+        } else {
+            fetch('https://api.monobank.ua/bank/currency')
+                .then(response => response.json())
+                .then(data => {
+                    let price = document.getElementsByClassName('price')[0];
+                    price.innerHTML = data[1].rateBuy;
+                })
+                .catch(err => {
+                    console.error("error fetching data : ", err);
+                });
+        }
+    }
+};
